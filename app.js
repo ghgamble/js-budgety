201d96c0da7c3d48964c27804712c4a0860ee398
Grace Gamble <grace.h.gamble@gmail.com>
2018-10-30 15:17:05 -0600

    Implementing the Module Pattern

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-/*
+var budgetController = (function(){
+      var x = 23;
+      var add = function(a) {
+            return x + a;
+      }
+      return {
+            publicTest: function(b) {
+                  return add(b);
+            }
+      }
+})();
 
-Plan for build
-1 - Add event handler
-2 - Get input values
-3 - Add the new item to our data structure
-4 - Add the new item to the UI
-5 - Calculate budget
-6 - Update the UI
+var UIController = (function(){
 
-Modules
-- Important aspect of any app's architecture
-- Keep the units of code for a project both cleanly separated and organized.
-- Encapsulate some data into privacy and expose other data publicly
+})();
 
-UI Module
-- Get input values
-- Add the new item to the UI
-- Update the UI
-
-Data Module
-- Add the new item to data structure
-- Calculate budget
-
-Controller Module
-- Add event handler
-
-*/
+var controller = (function(budgetCtrl, UICtrl){
+      var z = budgetCtrl.publicTest(5);
+      return {
+            anotherPublic: function() {
+                  console.log(z);
+            }
+      }
+})(budgetController, UIController);
