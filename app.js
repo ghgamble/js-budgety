1bc92e46951ca90e7b4bd0e25094c86d24a5a2a8
Grace Gamble <grace.h.gamble@gmail.com>
2018-10-30 14:58:36 -0600

    Created Plan for Build

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+
+Plan for build
+1 - Add event handler
+2 - Get input values
+3 - Add the new item to our data structure
+4 - Add the new item to the UI
+5 - Calculate budget
+6 - Update the UI
+
+Modules
+- Important aspect of any app's architecture
+- Keep the units of code for a project both cleanly separated and organized.
+- Encapsulate some data into privacy and expose other data publicly
+
+UI Module
+- Get input values
+- Add the new item to the UI
+- Update the UI
+
+Data Module
+- Add the new item to data structure
+- Calculate budget
+
+Controller Module
+- Add event handler
+
+*/
